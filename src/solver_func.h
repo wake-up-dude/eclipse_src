3a13e40c99a8ebf1f9bb2fff556833fe32ea24ef
wakeupnewbee <32076994+wakeupnewbee@users.noreply.github.com>
2017-10-12 14:23:40 -0400

    Update solver_func.h
    add the funciton update_hit
    used for when box.hit==0
    check if the remaining condition can solve the box itself

--- src/solver_func.h
+++ src/solver_func.h
@@ -24,6 +24,8 @@ void update_res(struct sudo box[][10], int x, int y, int x_y, int hit);
 
 int return_hit(int *Reserve_array, int number_left);
 
+void update_hit(int idx, int idy, int xory, int num);
+
 void update_Num_empty();
 
 #endif /* SOLVER_FUNC_H_ */
